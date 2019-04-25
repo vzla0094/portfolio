0fb42edd0aa888416ebc793c84feae7954699bda
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-24 17:04:32 -0700

    create-react-app and added sass

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mainContainer">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
