427380f695c17e889128cfed96c509496ff93dca
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-24 19:00:20 -0700

    finished with the navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="mainContainer">
+      <Navbar />
       <h1>Hello World</h1>
     </div>
   );
