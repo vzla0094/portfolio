e39d57ca06676cd340c082a9bc4ef5f22e80ba91
Eliezer Valenzuela <vzla0094@gmail.com>
2019-05-24 02:16:49 -0700

    Updates

---
+++ precache-manifest.a680dd059464da5b0cd252b85861960a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cd25be8fb709c767e173d674e15370",
+    "url": "/vzla0094/Portfolio/index.html"
+  },
+  {
+    "revision": "5165b1da38aed4bb9f2c",
+    "url": "/vzla0094/Portfolio/static/css/main.fd5ccade.chunk.css"
+  },
+  {
+    "revision": "ce9813084b34ed08ce62",
+    "url": "/vzla0094/Portfolio/static/js/2.df8eefd3.chunk.js"
+  },
+  {
+    "revision": "5165b1da38aed4bb9f2c",
+    "url": "/vzla0094/Portfolio/static/js/main.c5aff9b9.chunk.js"
+  },
+  {
+    "revision": "00317d2ef55d90571628",
+    "url": "/vzla0094/Portfolio/static/js/runtime~main.b6af7eac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vzla0094/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99b20e3409c3a0e6bb711fff2823da81",
+    "url": "/vzla0094/Portfolio/static/media/pic.99b20e34.jpg"
+  },
+  {
+    "revision": "e2570ae237281ccb9b87dbbc76fcf843",
+    "url": "/vzla0094/Portfolio/static/media/promedio.e2570ae2.png"
+  },
+  {
+    "revision": "a6429627c896d48421e868bdc0563f7e",
+    "url": "/vzla0094/Portfolio/static/media/rock-paper-scissors.a6429627.jpg"
+  },
+  {
+    "revision": "69a2932105dd5bf4af5243a3d966d3dd",
+    "url": "/vzla0094/Portfolio/static/media/talent-trade-mobile.69a29321.png"
+  },
+  {
+    "revision": "ef12536113455ae48d33e9923aa9fc66",
+    "url": "/vzla0094/Portfolio/static/media/talent-trade.ef125361.jpg"
+  }
+]);
