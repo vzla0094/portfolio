42f1c69e8b4bdf34be5406534a725528824ab41f
Eliezer Valenzuela <vzla0094@gmail.com>
2019-05-26 12:12:56 -0700

    added hoverable images for projects showcase

--- portfolio/src/components/sectionTwo/SectionTwo.js
+++ portfolio/src/components/sectionTwo/SectionTwo.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import talenttrade from "../../img/talent-trade.jpg";
+import talentTrade from "../../img/talent-trade.jpg";
+import rockPaperScissors from "../../img/rock-paper-scissors.jpg";
 class SectionTwo extends Component {
   state = {};
   render() {
@@ -11,21 +12,35 @@ class SectionTwo extends Component {
         </h2>
         <div className="sectionTwo__boxes">
           <div className="sectionTwo__boxes__highlight">
-            <MDBView hover zoom className="sectionTwo__boxes__highlight__cover">
-              <img
-                src={talenttrade}
-                className="sectionTwo__boxes__highlight__cover__img"
-                alt="talent-trade"
-              />
-              <MDBMask overlay="black-light" />
+            <MDBView
+              hover
+              zoom
+              className="sectionTwo__boxes__highlight__talentTrade z-depth-1-half"
+            >
+              <MDBMask overlay="black-light" className="flex-center">
+                <p className="sectionTwo__boxes__highlight__maskText">
+                  Tecnologías usadas: <br />
+                  React, Sass, React-Router
+                </p>
+              </MDBMask>
             </MDBView>
-            <div className="sectionTwo__boxes__highlight__two">
-              <a
-                href="https://vzla0094.github.io/rock_paper_scissors/"
-                target="_blank"
-                rel="noopener noreferrer"
-              />
-            </div>
+            <MDBView
+              hover
+              zoom
+              className="sectionTwo__boxes__highlight__rockPaperScissors z-depth-1-half"
+            >
+              {/* <img
+                src={rockPaperScissors}
+                className="sectionTwo__boxes__highlight__cover__img"
+                alt="rock-paper-scissors"
+              /> */}
+              <MDBMask overlay="black-light" className="flex-center">
+                <p className="sectionTwo__boxes__highlight__maskText">
+                  Tecnologías usadas: <br />
+                  CSS, Javascript
+                </p>
+              </MDBMask>
+            </MDBView>
           </div>
           <div className="sectionTwo__boxes__normal">
             <div className="sectionTwo__boxes__normal__one">
