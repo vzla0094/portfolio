b80e99ecbf37c5af34aa81e90b9197781f246212
Eliezer Valenzuela <vzla0094@gmail.com>
2019-05-26 17:35:05 -0700

    sectionThree & sectionFour responsive!

--- portfolio/src/components/sectionThree/SectionThree.js
+++ portfolio/src/components/sectionThree/SectionThree.js
@@ -26,9 +26,11 @@ class SectionThree extends Component {
             </p>
           </div>
         </div>
-        <a className="sectionThree__arrow" href="#sectionFour">
-          <img src={arrow} width="50" height="50" alt="arrow" />
-        </a>
+        <div className="sectionThree__arrow flex-center">
+          <a href="#sectionFour">
+            <img src={arrow} width="50" height="50" alt="arrow" />
+          </a>
+        </div>
       </div>
     );
   }
